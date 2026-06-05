f938eaac0e2eba55eebd62f4cb938229f3e5f3a4
AgusO25 <ojeda.agusd@gmail.com>
2026-06-05 19:02:23 -0300

    Agregando fotos de las peliculas

---
+++ src/data/imagenes.js
@@ -0,0 +1,36 @@
+// Aca voy a poner todas las imagenes, no conozco una manera de importar todas de una y hacerlo directamente en producto.js
+
+// Además quedaria todo muy largo.
+
+import alien from '../assets/Alien El Octavo Pasajero.webp';
+import padrino from '../assets/El Padrino.jpg';
+import resplandor from '../assets/El Resplandor.jpg';
+import anillos from '../assets/El Señor de los Anillos.jpg';
+import gladiador from '../assets/Gladiador.webp';
+import jurassic from '../assets/Jurassic Park.webp';
+import matrix from '../assets/Matrix.jpg';
+import supercool from '../assets/Supercool.webp';
+import terminator2 from '../assets/Terminator 2.jpg';
+import toyStory from '../assets/Toy Story.jpg';
+import vengadores from '../assets/Vengadores Infinity War.avif';
+import volverAlFuturo from '../assets/Volver al Futuro.jpg';
+
+
+
+
+
+export const imagenes = {
+    alien,
+    padrino,
+    resplandor,
+    anillos,
+    gladiador,
+    jurassic,
+    matrix,
+    supercool,
+    terminator2,
+    toyStory,
+    vengadores,
+    volverAlFuturo
+} 
+
--- src/data/productos.js
+++ src/data/productos.js
@@ -1,12 +1,15 @@
 // Volver al Futuro y El Resplandor los dejo en 0 para probar la función de Agotado.
 // URLs: dejamos placehold.co que generan imágenes automáticas con el nombre de la película desde su página, despues lo cambiamos con los assets.
+
+import { imagenes } from './imagenes';
+
 export const productos = [
   {
     id: 1,
     titulo: "Matrix",
     genero: "Ciencia Ficción",
     precio: 1500,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Matrix",
+    imagen: imagenes.matrix,
     sinopsis: "Un experto en computadoras descubre que su mundo es una simulación total creada con maliciosas intenciones por parte de la ciberinteligencia.",
     stock: 5
   },
@@ -15,7 +18,7 @@ export const productos = [
     titulo: "El Señor de los Anillos",
     genero: "Fantasía",
     precio: 1800,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=El+Señor+de+los+Anillos",
+    imagen: imagenes.anillos,
     sinopsis: "Un hobbit de la Comarca y ocho compañeros emprenden un viaje para destruir el poderoso Anillo Único y salvar la Tierra Media del Señor Oscuro Sauron.",
     stock: 3
   },
@@ -24,7 +27,7 @@ export const productos = [
     titulo: "Volver al Futuro",
     genero: "Ciencia Ficción",
     precio: 1200,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Volver+al+Futuro",
+    imagen: imagenes.volverAlFuturo,
     sinopsis: "Un joven de 17 años es enviado accidentalmente treinta años en el pasado en un DeLorean que viaja en el tiempo, inventado por su amigo científico.",
     stock: 0 // Sin stock para probar el bloqueo del botón
   },
@@ -33,7 +36,7 @@ export const productos = [
     titulo: "Jurassic Park",
     genero: "Aventura",
     precio: 1400,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Jurassic+Park",
+    imagen: imagenes.jurassic,
     sinopsis: "Un pragmático paleontólogo visita un parque temático casi completo en una isla donde han creado dinosaurios a partir de ADN de ámbar.",
     stock: 10
   },
@@ -42,7 +45,7 @@ export const productos = [
     titulo: "El Padrino",
     genero: "Drama",
     precio: 2000,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=El+Padrino",
+    imagen: imagenes.padrino,
     sinopsis: "El patriarca anciano de una dinastía del crimen organizado transfiere el control de su imperio clandestino a su hijo reacio.",
     stock: 2
   },
@@ -51,7 +54,7 @@ export const productos = [
     titulo: "Terminator 2",
     genero: "Acción",
     precio: 1300,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Terminator+2",
+    imagen: imagenes.terminator2,
     sinopsis: "Un cyborg, idéntico al que fracasó en asesinar a Sarah Connor, debe proteger a su hijo de un cyborg más avanzado.",
     stock: 7
   },
@@ -60,7 +63,7 @@ export const productos = [
     titulo: "Toy Story",
     genero: "Animación",
     precio: 1000,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Toy+Story",
+    imagen: imagenes.toyStory,
     sinopsis: "Un muñeco de un vaquero se siente profundamente amenazado y celoso cuando un nuevo muñeco de un astronauta se convierte en el juguete principal.",
     stock: 8
   },
@@ -69,7 +72,7 @@ export const productos = [
     titulo: "El Resplandor",
     genero: "Terror",
     precio: 1600,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=El+Resplandor",
+    imagen: imagenes.resplandor,
     sinopsis: "Una familia se dirige a un hotel aislado para el invierno, donde una presencia siniestra influye en el padre hacia la violencia.",
     stock: 0 // Sin stock para probar el bloqueo del botón
   },
@@ -78,7 +81,7 @@ export const productos = [
     titulo: "Gladiador",
     genero: "Acción",
     precio: 1500,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Gladiador",
+    imagen: imagenes.gladiador,
     sinopsis: "Un antiguo general romano es traicionado y su familia es asesinada por el hijo corrupto de un emperador, por lo que decide venir a Roma como gladiador.",
     stock: 6
   },
@@ -87,7 +90,7 @@ export const productos = [
     titulo: "Alien: El Octavo Pasajero",
     genero: "Terror",
     precio: 1400,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Alien",
+    imagen: imagenes.alien,
     sinopsis: "Después de que un buque mercante espacial recibe una transmisión desconocida como una llamada de socorro, aterriza en la luna de origen y encuentra algo espeluznante.",
     stock: 4
   },
@@ -96,7 +99,7 @@ export const productos = [
     titulo: "Supercool",
     genero: "Comedia",
     precio: 900,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Supercool",
+    imagen: imagenes.supercool,
     sinopsis: "Dos estudiantes de preparatoria co-dependientes se ven obligados a lidiar con la ansiedad por separación cuando su plan para organizar una fiesta sale mal.",
     stock: 12
   },
@@ -105,7 +108,7 @@ export const productos = [
     titulo: "Avengers: Infinity War",
     genero: "Acción",
     precio: 1900,
-    imagen: "https://placehold.co/300x450/1a1a1a/FFF?text=Avengers+:+Infinity+War",
+    imagen: imagenes.vengadores,
     sinopsis: "Los Vengadores y sus aliados deben estar dispuestos a sacrificarlo todo para derrotar al poderoso Thanos antes de que destruya el universo.",
     stock: 15
   }
