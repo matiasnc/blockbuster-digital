2b91c06e52c2c784a6f9103630b3fbc35f0b21af
matiasnc <mn.calderon97@gmail.com>
2026-06-11 12:39:37 -0300

    Cambio a color negro al boton

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -87,7 +87,7 @@ const Contacto = () => {
             </Form.Group>
 
             <Button 
-              variant="primary" 
+              variant="dark" 
               type="submit" 
               size="lg"
               className="w-100 fw-bold mt-2" 
